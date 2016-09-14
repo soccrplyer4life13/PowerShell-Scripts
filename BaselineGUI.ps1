6b1c256a3440f10804113414497ce64583bf3981
Casey Kolowinski <soccrplyer4life13@gmail.com>
2016-09-14 07:48:08 -0700

    Added Controls
    Added more example controls that can be used with the GUI

--- BaselineGUI.ps1
+++ BaselineGUI.ps1
@@ -14,7 +14,50 @@ $inputXML = @"
         <Button x:Name="btnGo" Content="Go" HorizontalAlignment="Left" Margin="251,258,0,0" VerticalAlignment="Top" Width="75"/>
         <TextBox x:Name="txtFirstOutput" HorizontalAlignment="Left" Height="23" Margin="112,186,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="120"/>
         <Label x:Name="lblOutputOne" Content="First Output:" HorizontalAlignment="Left" Margin="41,186,0,0" VerticalAlignment="Top"/>
-        <Button x:Name="btnClose" Content="Close" HorizontalAlignment="Left" Margin="331,258,0,0" VerticalAlignment="Top" Width="75"/>
+        <Button x:Name="btnClose" Content="Close" HorizontalAlignment="Left" Margin="331,258,0,0" VerticalAlignment="Top" Width="75"/>        
+        
+
+        <!-- OTHER CONTROLS THAT ARE POSSIBLE
+        --------------------------------------
+        <Border x:Name="border"/>
+        <Button x:Name="button"/>
+        <Calendar x:Name="calendar"/>
+        <Canvas x:Name="canvas"/>
+        <CheckBox x:Name="checkBox"/>
+        <ComboBox x:Name="combobox"/>
+        <ContentControl x:Name="contentControl"/>
+        <DataGrid x:Name="dataGrid"/>
+        <DatePicker x:Name="datepicker"/>
+        <DockPanel x:Name="dockpanel"/>
+        <DocumentViewer x:Name="documentViewer"/>
+        <Expander x:Name="expander"/>
+        <Frame x:Name="frame"/>
+        <Grid x:Name="grid"/>
+        <Image x:Name="image"/>
+        <InkCanvas x:Name="inkCanvas"/>
+        <ItemsControl x:Name="itemsControl"/>
+        <Label x:Name="label"/>
+        <Listbox x:Name="listbox"/>
+        <ListView x:Name="listview"/>
+        <Menu x:Name="menu"/>
+        <PasswordBox x:Name="passwordbox"/>
+        <ProgressBar x:Name="progressbar"/>
+        <RadioButton x:Name="radiobutton"/>
+        <RichTextBox x:Name="richtextbox"/>
+        <ScrollViewer x:Name="scrollviewer"/>
+        <Separator x:Name="separator"/>
+        <Slider x:Name="slider"/>
+        <StackPanel x:Name="stackpanel"/>
+        <TabControl x:Name="tabcontrol"/>
+        <TextBlock x:Name="textblock"/>
+        <TextBox x:Name="textbox"/>
+        <ToolBar x:Name="toolbar"/>
+        <TreeView x:Name="treeview"/>
+        <WrapPanel x:Name="wrappanel"/>
+
+        ---------------------------------------------------------------------------------------------------
+        FOR THE FULL LIST - https://msdn.microsoft.com/en-us/library/system.windows.controls(v=vs.110).aspx
+        -->
     </Grid>
 </Window>
 "@       
@@ -34,26 +77,26 @@ catch{Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax 
 # Load XAML Objects In PowerShell
 #===========================================================================
  
-$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "control_$($_.Name)" -Value $Form.FindName($_.Name)}
  
 Function Get-FormVariables{
 if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true}
 write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
-get-variable WPF*
+get-variable control_*
 }
  
-#Get-FormVariables
+Get-FormVariables
 #endregion
 
 #===========================================================================
-# Do your work here: append $WPF onto the name of your object. I.E. myTextBox becomes $WPFmyTextBox
+# Do your work here: append $control_ onto the name of your object. I.E. myTextBox becomes $control_myTextBox
 #=========================================================================== 
 
-$WPFbtnGo.Add_Click({
-    $inputText = $WPFtxtFirstInput.Text
-    $WPFtxtFirstOutput.Text = $inputText
+$control_btnGo.Add_Click({
+    $inputText = $control_txtFirstInput.Text
+    $control_txtFirstOutput.Text = $inputText
 })
-$WPFbtnClose.Add_Click({$form.Close()})
+$control_btnClose.Add_Click({$form.Close()})
 
 #===========================================================================
 # Shows the form
